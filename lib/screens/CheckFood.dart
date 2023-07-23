0d66199962258b88170e971229fe85e4e0d7fca1
Yi Chee <e0970589@u.nus.edu>
2023-07-24 06:32:15 +0800

    edited CheckFood to include the case where textfield input isnt present in the database

--- lib/screens/CheckFood.dart
+++ lib/screens/CheckFood.dart
@@ -1,3 +1,219 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+// import 'package:orbital_appllergy/Reusables/EmergencyCallButton.dart';
+// import 'package:orbital_appllergy/service/FirestoreService.dart';
+// import 'package:awesome_dialog/awesome_dialog.dart';
+//
+// //TODO: If have time, we can list all the allergens out and apply the tick/cross.
+//
+// class CheckFood extends StatefulWidget {
+//   const CheckFood({super.key});
+//
+//   @override
+//   State<CheckFood> createState() => _CheckFoodState();
+// }
+//
+// class _CheckFoodState extends State<CheckFood> {
+//
+//   final FireStoreService _fireStoreService = FireStoreService();
+//   final TextEditingController _foodName = TextEditingController();
+//
+//   @override
+//   void dispose() {
+//     _foodName.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         toolbarHeight: 70,
+//         title: const Text(
+//           "Check Food",
+//           style: TextStyle(
+//             fontSize: 32.0,
+//             fontFamily: 'Poppins',
+//             color: Colors.black,
+//           ),
+//         ),
+//         centerTitle: true,
+//         backgroundColor: Colors.red[100],
+//         actions: const [
+//           EmergencyCallButton(),
+//         ],
+//       ),
+//       body: CustomScrollView(
+//         slivers: [
+//           SliverStickyHeader(
+//             header: Container(
+//               color: Colors.red[200],
+//               padding: const EdgeInsets.all(10),
+//               child: const Text(
+//                 'Input food name to find out whether it contains your allergens!',
+//                 style: TextStyle(
+//                   fontFamily: 'Poppins',
+//                   fontSize: 16,
+//                   color: Colors.white,
+//                 ),
+//                 textAlign: TextAlign.center,
+//               ),
+//             ),
+//             sliver: SliverToBoxAdapter(
+//               child: Column(
+//                 children: [
+//                   const SizedBox(height: 50),
+//                   Container(
+//                     width: 300,
+//                     decoration: BoxDecoration(
+//                       border: Border.all(
+//                         color: Colors.red,
+//                         width: 1.0,
+//                       ),
+//                       borderRadius: BorderRadius.circular(8.0),
+//                     ),
+//                     child: TextFormField(
+//                       controller: _foodName,
+//                       decoration: const InputDecoration(
+//                         hintText: 'Enter food name here',
+//                         border: InputBorder.none,
+//                         contentPadding: EdgeInsets.symmetric(
+//                           horizontal: 10,
+//                           vertical: 14,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   const SizedBox(height: 30),
+//                   ElevatedButton(
+//                     style: ElevatedButton.styleFrom(
+//                       backgroundColor: Colors.red[400],
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(20),
+//                       ),
+//                     ),
+//                     onPressed: () async {
+//                       String foodName = _foodName.text.trim();
+//                       if (foodName.isEmpty) {
+//                         _buildAlertDialog(
+//                           context: context,
+//                           dialogType: DialogType.infoReverse,
+//                           title: 'Oops!',
+//                           desc: 'You forgot to enter a food name!'
+//                         ).show();
+//                         return;
+//                       }
+//                       bool result = await _fireStoreService.checkUserAllergenicFoods(foodName);
+//                       List<String> commonAllergens = await _fireStoreService
+//                           .findCommonAllergensForCheckFood(foodName);
+//                       if (context.mounted) {
+//                         if (result) {
+//                           _buildAlertDialog(
+//                             context: context,
+//                             dialogType: DialogType.warning,
+//                             title: 'Warning',
+//                             desc: '$foodName is probably a food you are allergic to, beware!'
+//                                 ' The allergens you have that are present in $foodName are: ',
+//                             allergens: commonAllergens,
+//                           ).show();
+//                         } else {
+//                           _buildAlertDialog(
+//                             context: context,
+//                             dialogType: DialogType.success,
+//                             title: 'Not Found!',
+//                             desc: 'Good news! ${_foodName.text} is probably not a food you are allergic to,'
+//                                 ' but proceed with caution just in case!',
+//                             // allergens: commonAllergens,
+//                           ).show();
+//                         }
+//                       }
+//                     },
+//                     child: const Text('Find out'),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+// AwesomeDialog _buildAlertDialog({
+//   required BuildContext context,
+//   required DialogType dialogType,
+//   required String? title,
+//   required String? desc,
+//   List<String>? allergens,
+// }) {
+//   return AwesomeDialog(
+//     context: context,
+//     dialogType: dialogType,
+//     borderSide: const BorderSide(
+//       color: Colors.green,
+//       width: 2,
+//     ),
+//     width: 280,
+//     buttonsBorderRadius: const BorderRadius.all(
+//       Radius.circular(2),
+//     ),
+//     animType: AnimType.bottomSlide,
+//     body: Column(
+//       children: [
+//         if (title != null) Text(
+//           title,
+//           style: const TextStyle(
+//             fontSize: 18,
+//             fontWeight: FontWeight.bold,
+//             fontFamily: 'Poppins',
+//           ),
+//         ),
+//         const SizedBox(height: 10),
+//         if (desc != null) Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 8.0),
+//           child: Text(
+//             desc,
+//             textAlign: TextAlign.left,
+//             style: const TextStyle(
+//               fontFamily: 'Poppins',
+//             ),
+//           ),
+//         ),
+//         const SizedBox(height: 10),
+//         /*
+//         Everything below will be shown only when the allergen parameter is not null.
+//         The spread operator ... is used before the square brackets [] to flatten
+//         the list of widgets and include them as children of the parent widget.
+//         This allows the conditional widgets to be added to the parent widget's children list.
+//          */
+//         if (allergens != null) ...[
+//           SizedBox(
+//             height: 200,
+//             child: ListView.builder(
+//                 padding: const EdgeInsets.only(top: 0), // Add top padding
+//                 itemCount: allergens.length,
+//                 itemBuilder: (context, index) {
+//                   return ListTile(
+//                       contentPadding: const EdgeInsets.only(left: 16),
+//                       title: Text(
+//                         '${index + 1}. ${allergens[index]}',
+//                         style: const TextStyle(
+//                           fontFamily: 'Poppins',
+//                           fontSize: 15,
+//                         ),
+//                       )
+//                   );
+//                 }
+//             ),
+//           ),
+//         ]
+//       ],
+//     ),
+//     showCloseIcon: true,
+//   );
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:orbital_appllergy/Reusables/EmergencyCallButton.dart';
@@ -14,7 +230,6 @@ class CheckFood extends StatefulWidget {
 }
 
 class _CheckFoodState extends State<CheckFood> {
-
   final FireStoreService _fireStoreService = FireStoreService();
   final TextEditingController _foodName = TextEditingController();
 
@@ -99,13 +314,31 @@ class _CheckFoodState extends State<CheckFood> {
                           context: context,
                           dialogType: DialogType.infoReverse,
                           title: 'Oops!',
-                          desc: 'You forgot to enter a food name!'
+                          desc: 'You forgot to enter a food name!',
                         ).show();
                         return;
                       }
+
+                      // Check if the food name is present in the common allergens list
+                      bool isCommonAllergen = false;
+                      List<String> commonAllergens = await _fireStoreService.findCommonAllergensForCheckFood(foodName);
+                      if (commonAllergens.isNotEmpty) {
+                        isCommonAllergen = true;
+                      }
+
+                      if (!isCommonAllergen) {
+                        _buildAlertDialog(
+                          context: context,
+                          dialogType: DialogType.warning,
+                          title: 'Warning',
+                          desc: 'We were unable to check and cannot give you a definite answer now.'
+                              ' Do proceed with caution!',
+                        ).show();
+                        return;
+                      }
+
                       bool result = await _fireStoreService.checkUserAllergenicFoods(foodName);
-                      List<String> commonAllergens = await _fireStoreService
-                          .findCommonAllergensForCheckFood(foodName);
+                      List<String> commonAllergen = await _fireStoreService.findCommonAllergensForCheckFood(foodName);
                       if (context.mounted) {
                         if (result) {
                           _buildAlertDialog(
@@ -114,7 +347,7 @@ class _CheckFoodState extends State<CheckFood> {
                             title: 'Warning',
                             desc: '$foodName is probably a food you are allergic to, beware!'
                                 ' The allergens you have that are present in $foodName are: ',
-                            allergens: commonAllergens,
+                            allergens: commonAllergen,
                           ).show();
                         } else {
                           _buildAlertDialog(
@@ -213,4 +446,3 @@ AwesomeDialog _buildAlertDialog({
     showCloseIcon: true,
   );
 }
-
